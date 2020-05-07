3dd03995cfa529dbe6331094ee58f6029f8ed4e3
vodopyanov ilya <vodopyanov999@gmail.com>
2020-05-07 14:17:14 +0300

    final

--- alice1.py
+++ alice1.py
@@ -1,35 +1,15 @@
-# импортируем библиотеки
 from flask import Flask, request
 import logging
 import pymorphy2
 
-# библиотека, которая нам понадобится для работы с JSON
 import json
-
 import os
 
-# создаём приложение
-# мы передаём __name__, в нем содержится информация,
-# в каком модуле мы находимся.
-# В данном случае там содержится '__main__',
-# так как мы обращаемся к переменной из запущенного модуля.
-# если бы такое обращение, например,
-# произошло внутри модуля logging, то мы бы получили 'logging'
+
 app = Flask(__name__)
 
-# Устанавливаем уровень логирования
 logging.basicConfig(level=logging.INFO)
 
-# Создадим словарь, чтобы для каждой сессии общения
-# с навыком хранились подсказки, которые видел пользователь.
-# Это поможет нам немного разнообразить подсказки ответов
-# (buttons в JSON ответа).
-# Когда новый пользователь напишет нашему навыку,
-# то мы сохраним в этот словарь запись формата
-# sessionStorage[user_id] = {'suggests': ["Не хочу.", "Не буду.", "Отстань!" ]}
-# Такая запись говорит, что мы показали пользователю эти три подсказки.
-# Когда он откажется купить слона,
-# то мы уберем одну подсказку. Как будто что-то меняется :)
 sessionStorage = {}
 
 animals = [
@@ -41,15 +21,9 @@ morph = pymorphy2.MorphAnalyzer()
 
 
 @app.route('/post', methods=['POST'])
-# Функция получает тело запроса и возвращает ответ.
-# Внутри функции доступен request.json - это JSON,
-# который отправила нам Алиса в запросе POST
 def main():
     logging.info(f'Request: {request.json!r}')
 
-    # Начинаем формировать ответ, согласно документации
-    # мы собираем словарь, который потом при помощи
-    # библиотеки json преобразуем в JSON и отдадим Алисе
     response = {
         'session': request.json['session'],
         'version': request.json['version'],
@@ -58,14 +32,10 @@ def main():
         }
     }
 
-    # Отправляем request.json и response в функцию handle_dialog.
-    # Она сформирует оставшиеся поля JSON, которые отвечают
-    # непосредственно за ведение диалога
     handle_dialog(request.json, response)
 
     logging.info(f'Response:  {response!r}')
 
-    # Преобразовываем в JSON и возвращаем
     return json.dumps(response)
 
 
@@ -73,9 +43,6 @@ def handle_dialog(req, res):
     user_id = req['session']['user_id']
 
     if req['session']['new']:
-        # Это новый пользователь.
-        # Инициализируем сессию и поприветствуем его.
-        # Запишем подсказки, которые мы ему покажем в первый раз
 
         sessionStorage[user_id] = {
             'suggests': [
@@ -85,28 +52,17 @@ def handle_dialog(req, res):
             ],
             'animal_index': 0
         }
-        # Заполняем текст ответа
         animal = animals[sessionStorage[user_id]['animal_index']]
         parse = morph.parse(animal)[0]
         word = parse.inflect({'sing', 'gent'}).word
         res['response']['text'] = f'Привет! Купи {word}!'
-        # Получим подсказки
         res['response']['buttons'] = get_suggests(user_id)
         return
 
-    # Сюда дойдем только, если пользователь не новый,
-    # и разговор с Алисой уже был начат
-    # Обрабатываем ответ пользователя.
-    # В req['request']['original_utterance'] лежит весь текст,
-    # что нам прислал пользователь
-    # Если он написал 'ладно', 'куплю', 'покупаю', 'хорошо',
-    # то мы считаем, что пользователь согласился.
-    # Подумайте, всё ли в этом фрагменте написано "красиво"?
-    if any(map(lambda word: word in req['request']['original_utterance'].lower(), ['ладно',
-                                                                                   'куплю',
-                                                                                   'покупаю',
-                                                                                   'хорошо'])):
-        # Пользователь согласился, прощаемся.
+    if any(map(lambda w: w in req['request']['original_utterance'].lower(), ['ладно',
+                                                                             'куплю',
+                                                                             'покупаю',
+                                                                             'хорошо'])):
         animal = animals[sessionStorage[user_id]['animal_index']]
         parse = morph.parse(animal)[0]
         word = parse.inflect({'sing', 'gent'}).word
@@ -131,7 +87,6 @@ def handle_dialog(req, res):
             res['response']['text'] += f'\nА теперь купи {word}!'
             res['response']['buttons'] = get_suggests(user_id)
     else:
-        # Если нет, то убеждаем его купить слона!
         animal = animals[sessionStorage[user_id]['animal_index']]
         parse = morph.parse(animal)[0]
         word = parse.inflect({'sing', 'gent'}).word
@@ -140,22 +95,17 @@ def handle_dialog(req, res):
         res['response']['buttons'] = get_suggests(user_id)
 
 
-# Функция возвращает две подсказки для ответа.
 def get_suggests(user_id):
     session = sessionStorage[user_id]
 
-    # Выбираем две первые подсказки из массива.
     suggests = [
         {'title': suggest, 'hide': True}
         for suggest in session['suggests'][:2]
     ]
 
-    # Убираем первую подсказку, чтобы подсказки менялись каждый раз.
     session['suggests'] = session['suggests'][1:]
     sessionStorage[user_id] = session
 
-    # Если осталась только одна подсказка, предлагаем подсказку
-    # со ссылкой на Яндекс.Маркет.
     if len(suggests) < 2:
         animal = animals[sessionStorage[user_id]['animal_index']]
         suggests.append({
@@ -168,5 +118,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
